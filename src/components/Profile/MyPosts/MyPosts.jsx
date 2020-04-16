177f108ae8f6e193275e0b9cf2af44d86b3935e8
Max Ulshin <max.ulshin.max@istat.com.ua>
2020-04-16 10:46:26 +0300

    lesson 35

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,26 +2,39 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () => {
-  return (
-    <div>
-      My posts
-      <div>
-        <textarea></textarea>
-        <button>Add post</button>
+const MyPosts = (props) => {
+    let postsElements =
+        props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>);
 
-      </div>
-      <div className={s.posts}>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
-    </div>
-  )
+    let newPostElement = React.createRef();
 
+    let addPost = () => {
+        // let text = newPostElement.current.value;
+        props.addPost(props.newPostText);
+    }
+
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        console.log(text);
+        props.updateNewPostText(text);
+    }
+
+    return (
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div>
+                <div>
+                    <textarea ref={newPostElement} value={props.newPostText} onChange={onPostChange}/>
+                </div>
+                <div>
+                    <button onClick={addPost}>Add post</button>
+                </div>
+            </div>
+            <div className={s.posts}>
+                { postsElements }
+            </div>
+        </div>
+    )
 }
 
 export default MyPosts;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {BrowserRouter} from "react-router-dom";
+// import {posts, messages, dialogs} from './redux/state'; для не дефолтного експорта надо куча приемников и расширения файла а  не путь к модулю
+import state from './redux/state';
+import {addPost, updateNewPostText, callback_tree} from './redux/state.js';
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+let renderEntireTree = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+      <App state={state} addPost={addPost}  updateNewPostText={updateNewPostText}/>
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+renderEntireTree();
+callback_tree(renderEntireTree)
+// renderEntireTree(state, addPost, updateNewPostText);
+
 serviceWorker.unregister();
